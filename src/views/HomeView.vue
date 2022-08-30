081cbfd5c4c203c666e1b9952cc25330c3bbef58
lifei <1342857386@qq.com>
2022-08-30 19:24:04 +0800

    全局引入elementui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,14 @@
   <div class="hello">
     <input id="inputArea" type="text">
     <button @click="sendMsgToServer">发送消息给服务端</button>
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
   </div>
 </template>
 
